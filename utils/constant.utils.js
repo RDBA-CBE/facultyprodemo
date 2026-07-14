c5e7ef4be247fb6491ff25815f25e98e16bc5f6d
Duraisamy303 <psmkduraisamy@gmail.com>
2026-07-14 12:44:11 +0530

    Base URl updated

--- utils/constant.utils.js
+++ utils/constant.utils.js
@@ -1,4 +1,4 @@
-export const BASEURL = "https://user-service.88.222.213.249.nip.io/api/";
+export const BASEURL = "https://demofile.facultypro.in/api/";
 
 export const CLIENT_ID =
   "625052261407-4p8ihs05c67d778mr5d91lqjvnvpkd8k.apps.googleusercontent.com";
